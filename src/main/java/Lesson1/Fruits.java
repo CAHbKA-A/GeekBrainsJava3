ad5a7db4cc57f70007f88434504f49ee5790be22
Student <123@12d.dd>
2021-03-21 21:46:18 +0800

    Lesson1_HW

---
+++ src/main/java/Lesson1/Fruits.java
@@ -0,0 +1,19 @@
+package Lesson1;
+
+public class Fruits {
+    private double weight;
+   // private int count;
+
+    public Fruits(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+}
---
+++ src/main/java/Lesson1/Orange.java
@@ -0,0 +1,8 @@
+package Lesson1;
+
+public class Orange extends Fruits {
+
+    public Orange() {
+        super(1.5f);
+    }
+}
