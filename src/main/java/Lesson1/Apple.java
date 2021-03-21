0e5193af0a965d2e04ff5377b0871a2235cd6506
Student <123@12d.dd>
2021-03-21 21:55:47 +0800

    test

--- src/main/java/Lesson1/Apple.java
+++
@@ -1,8 +0,0 @@
-package Lesson1;
-
-public class Apple extends Fruits {
-
-    public Apple() {
-        super(1.0f);
-    }
-}
--- src/main/java/Lesson1/Orange.java
+++
@@ -1,8 +0,0 @@
-package Lesson1;
-
-public class Orange extends Fruits {
-
-    public Orange() {
-        super(1.5f);
-    }
-}
