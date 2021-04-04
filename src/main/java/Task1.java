f6c3df6b7fb16ee6acc602a77a4af6ab07ff9036
Student <123@12d.dd>
2021-04-04 16:35:52 +0800

    /*Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС). Используйте wait/notify/notifyAll./*

---
+++ src/main/java/Task1.java
@@ -0,0 +1,45 @@
+/*Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз
+(порядок – ABСABСABС). Используйте wait/notify/notifyAll./*
+ */
+public class Task1 {
+
+    private static final Object monitor = new Object();
+    private static char letter = 'C';
+
+    public static void main(String[] args) {
+
+        Task1 taskThread= new Task1();
+
+        new Thread(() -> taskThread.print('A')).start();
+        new Thread(() -> taskThread.print('B')).start();
+        new Thread(() -> taskThread.print('C')).start();
+    }
+
+    public void print(char c) {
+        char c1 = nextChar(c);
+
+        try {
+            for (int i = 0; i < 15; i++) {
+                synchronized (monitor) {
+                    if (letter != c)
+                        monitor.wait();
+                    System.out.print(c1);
+                    letter = c1;
+                    monitor.notify();
+                }
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public char nextChar(char c) {
+        if (c == 'A') return 'B';
+        if (c == 'B') return 'C';
+        return 'A';
+    }
+
+
+}
